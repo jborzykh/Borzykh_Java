8c9962ad68f303871f418daa7d3839dc807499d8
jborzykh <jborzykh@mail.ru>
2022-10-21 18:55:59 +0300

    why so negative

--- src/main/java/Divide.java
+++ src/main/java/Divide.java
@@ -3,18 +3,23 @@ import java.util.Scanner;
 public class Divide {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the length of your Array:");
-        int len = scanner.nextInt();
-        int[] mas = new int[len];
-        for (int i = 0; i < mas.length; i++) {
-            mas[i] = scanner.nextInt();
-        }
-
-
-        for (int i = 0; i < mas.length; i++) {
-            if (mas[i] % 3 == 0 & mas[i] != 0) {
-                System.out.print(mas[i] + " ");
+        try {
+            System.out.println("Enter the length of your Array:");
+            int len = scanner.nextInt();
+            int[] mas = new int[len];
+            for (int i = 0; i < mas.length; i++) {
+                mas[i] = scanner.nextInt();
             }
+
+
+            for (int i = 0; i < mas.length; i++) {
+                if (mas[i] % 3 == 0 & mas[i] != 0) {
+                    System.out.print(mas[i] + " ");
+                }
+            }
+
+        } catch (NegativeArraySizeException var8) {
+            System.out.println("Seriously? Why so negative?");
         }
     }
 }
